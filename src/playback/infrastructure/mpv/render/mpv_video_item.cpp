ea60674a70a3ad0502d6ac7053b82d78735adb0f
uniquenesssta <uniquenesssta@live.com>
2026-08-10 02:05:52 +0800

    R4-04: implement video item state capture

---
+++ src/playback/infrastructure/mpv/render/mpv_video_item.cpp
@@ -0,0 +1,33 @@
+#include "mpv_video_item.h"
+
+#include "mpv_video_renderer.h"
+
+#include <QQuickWindow>
+
+namespace player::playback::infrastructure::mpv::render {
+
+MpvVideoItem::MpvVideoItem(QQuickItem* parent)
+    : QQuickFramebufferObject(parent)
+{
+}
+
+QQuickFramebufferObject::Renderer* MpvVideoItem::createRenderer() const
+{
+    return new MpvVideoRenderer();
+}
+
+MpvVideoPresentationState MpvVideoItem::presentationState() const noexcept
+{
+    const QQuickWindow* quickWindow = window();
+    const qreal devicePixelRatio = quickWindow != nullptr
+        ? quickWindow->devicePixelRatio()
+        : qreal{1.0};
+
+    return MpvVideoPresentationState{
+        QSizeF{width(), height()},
+        devicePixelRatio,
+        isVisible(),
+    };
+}
+
+} // namespace player::playback::infrastructure::mpv::render
