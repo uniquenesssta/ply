740b0172ab1605e02614ce9eb001cbd88e305260
uniquenesssta <uniquenesssta@live.com>
2026-08-10 02:05:44 +0800

    R4-04: add QML video item boundary

---
+++ src/playback/infrastructure/mpv/render/mpv_video_item.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <QQuickFramebufferObject>
+#include <QSizeF>
+
+namespace player::playback::infrastructure::mpv::render {
+
+struct MpvVideoPresentationState final
+{
+    QSizeF logicalSize;
+    qreal devicePixelRatio = 1.0;
+    bool visible = false;
+
+    bool operator==(const MpvVideoPresentationState&) const = default;
+};
+
+class MpvVideoItem final : public QQuickFramebufferObject
+{
+    Q_OBJECT
+
+public:
+    explicit MpvVideoItem(QQuickItem* parent = nullptr);
+
+    [[nodiscard]] Renderer* createRenderer() const override;
+    [[nodiscard]] MpvVideoPresentationState presentationState() const noexcept;
+};
+
+} // namespace player::playback::infrastructure::mpv::render
