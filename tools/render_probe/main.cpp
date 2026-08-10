3b77b6dc34c26878a273a30c1a03c214f57f8d31
uniquenesssta <uniquenesssta@live.com>
2026-08-11 00:40:45 +0800

    fix: keep R4-09 probe alive through window transitions

--- tools/render_probe/main.cpp
+++ tools/render_probe/main.cpp
@@ -17,6 +17,7 @@ int main(int argc, char* argv[])
 
     QGuiApplication application(argc, argv);
     QCoreApplication::setApplicationName(QStringLiteral("render_probe"));
+    application.setQuitOnLastWindowClosed(false);
 
     QString parseError;
     const auto options = RenderProbeOptionsParser::parse(
