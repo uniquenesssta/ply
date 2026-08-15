0f8f084c77538c51a93ffff06ac6028e0eddc4f0
uniquenesssta <uniquenesssta@live.com>
2026-08-15 21:39:16 +0800

    feat: add media source domain value

---
+++ src/media/domain/media_source.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <QString>
+#include <QtGlobal>
+
+namespace player::media::domain {
+
+enum class MediaSourceKind : quint8
+{
+    LocalFile = 0,
+};
+
+class MediaSource final
+{
+public:
+    [[nodiscard]] static MediaSource localFile(QString canonicalPath);
+
+    [[nodiscard]] MediaSourceKind kind() const noexcept;
+    [[nodiscard]] const QString& location() const noexcept;
+    [[nodiscard]] bool isValid() const noexcept;
+
+private:
+    MediaSource(MediaSourceKind kind, QString location);
+
+    MediaSourceKind kind_ = MediaSourceKind::LocalFile;
+    QString location_;
+};
+
+} // namespace player::media::domain
