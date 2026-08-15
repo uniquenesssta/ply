d4867e22ed4046a365f87a2b488253ccbdec15dd
uniquenesssta <uniquenesssta@live.com>
2026-08-15 21:39:45 +0800

    feat: add media open coordinator

---
+++ src/media/application/open/media_open_error.cpp
@@ -0,0 +1,29 @@
+#include "media/application/open/media_open_error.h"
+
+#include <QString>
+
+namespace player::media::application {
+
+QString mediaOpenErrorKey(MediaOpenError error)
+{
+    switch (error) {
+    case MediaOpenError::None:
+        return {};
+    case MediaOpenError::NotLocalFile:
+        return QStringLiteral("not-local-file");
+    case MediaOpenError::NotFound:
+        return QStringLiteral("not-found");
+    case MediaOpenError::NotRegularFile:
+        return QStringLiteral("not-regular-file");
+    case MediaOpenError::NotReadable:
+        return QStringLiteral("not-readable");
+    case MediaOpenError::CanonicalizationFailed:
+        return QStringLiteral("canonicalization-failed");
+    case MediaOpenError::SubmissionRejected:
+        return QStringLiteral("submission-rejected");
+    }
+
+    return QStringLiteral("unknown");
+}
+
+} // namespace player::media::application
