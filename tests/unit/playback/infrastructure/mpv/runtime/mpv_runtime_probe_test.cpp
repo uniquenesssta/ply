d2ef3e6d26fc092cf9b4077927ba9c94b2f91e60
uniquenesssta <uniquenesssta@live.com>
2026-08-16 18:09:08 +0800

    test: lock HTTPS TLS package policy

--- tests/unit/playback/infrastructure/mpv/runtime/mpv_runtime_probe_test.cpp
+++ tests/unit/playback/infrastructure/mpv/runtime/mpv_runtime_probe_test.cpp
@@ -1,6 +1,10 @@
 #include "playback/infrastructure/mpv/runtime/mpv_runtime_probe.h"
 
+#include <QFile>
 #include <QFileInfo>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QJsonObject>
 #include <QtTest>
 
 namespace player::playback::mpv {
@@ -11,6 +15,7 @@ class MpvRuntimeProbeTest final : public QObject
 
 private slots:
     void validatesStagedRuntimeAndManifest();
+    void manifestRequiresWindowsTlsForHttpsMedia();
 };
 
 void MpvRuntimeProbeTest::validatesStagedRuntimeAndManifest()
@@ -26,6 +31,30 @@ void MpvRuntimeProbeTest::validatesStagedRuntimeAndManifest()
     QCOMPARE(info.manifest.mpvCommit, QStringLiteral(PLAYER_EXPECTED_MPV_COMMIT));
     QCOMPARE(info.manifest.ffmpegVersion, QStringLiteral(PLAYER_EXPECTED_FFMPEG_VERSION));
     QVERIFY(info.clientApiMajor > 0);
+}
+
+void MpvRuntimeProbeTest::manifestRequiresWindowsTlsForHttpsMedia()
+{
+    MpvRuntimeInfo info;
+    QString errorMessage;
+    QVERIFY2(MpvRuntimeProbe::probe(info, &errorMessage), qPrintable(errorMessage));
+
+    QFile manifestFile(info.manifestPath);
+    QVERIFY2(manifestFile.open(QIODevice::ReadOnly), qPrintable(manifestFile.errorString()));
+
+    QJsonParseError parseError;
+    const QJsonDocument document = QJsonDocument::fromJson(manifestFile.readAll(), &parseError);
+    QCOMPARE(parseError.error, QJsonParseError::NoError);
+    QVERIFY(document.isObject());
+
+    const QJsonArray ffmpegPolicy = document.object()
+                                         .value(QStringLiteral("buildPolicy"))
+                                         .toObject()
+                                         .value(QStringLiteral("ffmpeg"))
+                                         .toArray();
+    QVERIFY2(
+        ffmpegPolicy.contains(QStringLiteral("enable-schannel")),
+        "The audited Windows FFmpeg package must explicitly enable Schannel when autodetect is disabled so HTTPS media is supported.");
 }
 
 } // namespace player::playback::mpv
