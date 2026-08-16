6a9b2ab491b0b4a499f4424b357d674a3a9ca9fb
uniquenesssta <uniquenesssta@live.com>
2026-08-16 11:58:46 +0800

    diag: log product render context activation

--- src/playback/infrastructure/mpv/render/mpv_video_renderer.cpp
+++ src/playback/infrastructure/mpv/render/mpv_video_renderer.cpp
@@ -1,11 +1,13 @@
 #include "mpv_video_renderer.h"
 
+#include "foundation/logging/log_categories.h"
 #include "mpv_render_context.h"
 #include "mpv_render_parameters.h"
 #include "mpv_render_shutdown_coordinator.h"
 #include "mpv_render_update_bridge.h"
 
 #include <QDebug>
+#include <QLoggingCategory>
 #include <QOpenGLContext>
 #include <QOpenGLFramebufferObject>
 #include <QOpenGLFunctions>
@@ -271,6 +273,8 @@ bool MpvVideoRenderer::ensureRenderContext()
     }
 
     renderContext_ = std::move(renderContext);
+    qCInfo(player::logging::playbackRender)
+        << "Product mpv video render context active";
     return true;
 }
 
