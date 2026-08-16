b582dbc1496d3a186712ba073f045dd24727d2cf
uniquenesssta <uniquenesssta@live.com>
2026-08-16 19:21:43 +0800

    feat: add R7-04 argument open workflow

---
+++ src/media/application/arguments/media_argument_open_workflow.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <QList>
+#include <QString>
+#include <QStringList>
+#include <QUrl>
+#include <QtGlobal>
+
+namespace player::media::application {
+
+class MediaOpenCoordinator;
+class UrlOpenWorkflow;
+
+enum class MediaArgumentOpenOutcome : quint8
+{
+    Ignored = 0,
+    OpenedLocal,
+    OpenedRemote,
+    DeferredMultiple,
+    RejectedLocal,
+    RejectedRemote,
+    RejectedUnsupported,
+};
+
+struct MediaArgumentOpenResult final
+{
+    MediaArgumentOpenOutcome outcome = MediaArgumentOpenOutcome::Ignored;
+    QList<QUrl> orderedSources;
+
+    [[nodiscard]] bool opened() const noexcept;
+    [[nodiscard]] bool deferred() const noexcept;
+};
+
+class MediaArgumentOpenWorkflow final
+{
+public:
+    MediaArgumentOpenWorkflow(
+        MediaOpenCoordinator& mediaOpenCoordinator,
+        UrlOpenWorkflow& urlOpenWorkflow);
+
+    [[nodiscard]] MediaArgumentOpenResult openProcessArguments(
+        const QStringList& processArguments,
+        const QString& workingDirectory);
+
+private:
+    MediaOpenCoordinator& mediaOpenCoordinator_;
+    UrlOpenWorkflow& urlOpenWorkflow_;
+};
+
+} // namespace player::media::application
