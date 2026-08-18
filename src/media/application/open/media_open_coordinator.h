c41c17bbdf45c8e06cd08a74eb9ded78cec5b8e7
uniquenesssta <uniquenesssta@live.com>
2026-08-18 16:37:31 +0800

    r7-13: expose media open supersession boundary

--- src/media/application/open/media_open_coordinator.h
+++ src/media/application/open/media_open_coordinator.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "media/application/open/media_open_error.h"
+#include "media/application/open/media_open_operation_id.h"
 #include "media/domain/media_source.h"
 
 #include <QList>
@@ -30,6 +31,19 @@ public:
 
     [[nodiscard]] QString lastErrorKey() const;
 
+    [[nodiscard]] MediaOpenOperationId beginReplaceOpenOperation() noexcept;
+    [[nodiscard]] bool isOpenOperationCurrent(MediaOpenOperationId operationId) const noexcept;
+    [[nodiscard]] bool cancelOpenOperation(MediaOpenOperationId operationId) noexcept;
+    [[nodiscard]] bool acceptingOpenOperations() const noexcept;
+    void beginShutdown() noexcept;
+
+    [[nodiscard]] bool completeOpenSource(
+        MediaOpenOperationId operationId,
+        const player::media::domain::MediaSource& source);
+    [[nodiscard]] bool completeOpenSources(
+        MediaOpenOperationId operationId,
+        const QList<player::media::domain::MediaSource>& sources);
+
     [[nodiscard]] bool openSource(const player::media::domain::MediaSource& source);
     [[nodiscard]] bool openSources(
         const QList<player::media::domain::MediaSource>& sources);
@@ -42,11 +56,22 @@ signals:
     void openRejected(const QString& errorKey);
 
 private:
+    [[nodiscard]] bool consumeOpenOperation(MediaOpenOperationId operationId) noexcept;
+    [[nodiscard]] bool rejectOpenOperation(
+        MediaOpenOperationId operationId,
+        MediaOpenError error);
+    [[nodiscard]] bool submitSource(
+        const player::media::domain::MediaSource& source);
+    [[nodiscard]] bool submitSources(
+        const QList<player::media::domain::MediaSource>& sources);
     void setError(MediaOpenError error);
 
     SubmitMedia submitMedia_;
     SubmitMediaBatch submitMediaBatch_;
     MediaOpenError lastError_ = MediaOpenError::None;
+    quint64 nextOperationValue_ = 1;
+    MediaOpenOperationId currentOperation_;
+    bool acceptingOpenOperations_ = true;
 };
 
 } // namespace player::media::application
