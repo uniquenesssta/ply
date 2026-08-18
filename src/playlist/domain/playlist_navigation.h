15e4ad8780b5a75b31d3a3e433faf9f833c1fbe8
uniquenesssta <uniquenesssta@live.com>
2026-08-18 13:57:35 +0800

    r7-10: derive queue navigation capabilities

--- src/playlist/domain/playlist_navigation.h
+++ src/playlist/domain/playlist_navigation.h
@@ -7,6 +7,7 @@
 namespace player::playlist::domain {
 
 class Playlist;
+class PlaylistSnapshot;
 
 enum class PlaylistNavigationAction : quint8
 {
@@ -22,9 +23,17 @@ struct PlaylistNavigationDecision final
     PlaylistEntryId targetEntryId;
 };
 
+struct PlaylistNavigationCapabilities final
+{
+    bool canNext = false;
+    bool canPrevious = false;
+};
+
 class PlaylistNavigation final
 {
 public:
+    [[nodiscard]] static PlaylistNavigationCapabilities capabilities(
+        const PlaylistSnapshot& snapshot) noexcept;
     [[nodiscard]] static PlaylistNavigationDecision afterNaturalEnd(
         Playlist& playlist);
     [[nodiscard]] static PlaylistNavigationDecision afterPlaybackFailure(
