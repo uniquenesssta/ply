432495cf02fe6bcaae2028671c62e40a70655c92
uniquenesssta <uniquenesssta@live.com>
2026-08-18 15:50:28 +0800

    r7-12: gate auto advance by observed generation

--- src/playlist/application/playlist_auto_advance.h
+++ src/playlist/application/playlist_auto_advance.h
@@ -27,6 +27,7 @@ public:
 
     void acceptPlaybackState(quint64 mediaGeneration, bool naturallyEnded);
     void acceptPlaybackFailure(quint64 mediaGeneration);
+    void suppressObservedGeneration() noexcept;
 
 private:
     enum class TerminalReason : quint8
@@ -35,10 +36,13 @@ private:
         Failure,
     };
 
+    [[nodiscard]] bool observeGeneration(quint64 mediaGeneration) noexcept;
     void acceptTerminalState(quint64 mediaGeneration, TerminalReason reason);
 
     domain::Playlist& playlist_;
     PlaylistController& controller_;
+    std::optional<quint64> observedGeneration_;
+    std::optional<quint64> supersededGeneration_;
     std::optional<quint64> handledTerminalGeneration_;
 };
 
