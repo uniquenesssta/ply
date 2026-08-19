367a219cf8992719d8fb0acf29ec21cd18fa2871
uniquenesssta <uniquenesssta@live.com>
2026-08-19 16:56:19 +0800

    r8-05: expose subtitle delay controller to QML

--- src/app/bootstrap/application_bootstrap.cpp
+++ src/app/bootstrap/application_bootstrap.cpp
@@ -21,6 +21,7 @@
 #include "presentation/viewmodels/player/transport/player_transport_view_model.h"
 #include "presentation/viewmodels/player/volume/player_volume_view_model.h"
 #include "tracks/application/external_subtitle_loader.h"
+#include "tracks/application/subtitle_delay_controller.h"
 #include "tracks/application/track_selection_controller.h"
 #include "tracks/presentation/track_list_model.h"
 
@@ -150,6 +151,10 @@ int ApplicationBootstrap::run(
         QStringLiteral("externalSubtitleLoader"),
         QVariant::fromValue(
             static_cast<QObject*>(&container.externalSubtitleLoader())));
+    initialProperties.insert(
+        QStringLiteral("subtitleDelayController"),
+        QVariant::fromValue(
+            static_cast<QObject*>(&container.subtitleDelayController())));
     initialProperties.insert(
         QStringLiteral("audioTrackModel"),
         QVariant::fromValue(
