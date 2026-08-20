b4d9ab78377a4f37c98f107dc26f54cdd294d344
uniquenesssta <uniquenesssta@live.com>
2026-08-20 11:02:44 +0800

    R8-07 point composition at chapters module

--- src/app/composition/application_container.h
+++ src/app/composition/application_container.h
@@ -4,6 +4,10 @@
 
 #include <memory>
 
+namespace player::chapters::presentation {
+class ChapterModel;
+}
+
 namespace player::media::application {
 class MediaArgumentOpenWorkflow;
 class MediaDropHandler;
@@ -35,7 +39,6 @@ class TrackSelectionController;
 }
 
 namespace player::tracks::presentation {
-class ChapterModel;
 class TrackListModel;
 }
 
@@ -70,7 +73,7 @@ public:
     [[nodiscard]] player::tracks::application::AudioDelayController& audioDelayController() noexcept;
     [[nodiscard]] player::tracks::presentation::TrackListModel& audioTrackListModel() noexcept;
     [[nodiscard]] player::tracks::presentation::TrackListModel& subtitleTrackListModel() noexcept;
-    [[nodiscard]] player::tracks::presentation::ChapterModel& chapterModel() noexcept;
+    [[nodiscard]] player::chapters::presentation::ChapterModel& chapterModel() noexcept;
     [[nodiscard]] player::media::application::MediaOpenCoordinator& mediaOpenCoordinator() noexcept;
     [[nodiscard]] player::media::application::UrlOpenWorkflow& urlOpenWorkflow() noexcept;
     [[nodiscard]] player::media::application::MediaArgumentOpenWorkflow& mediaArgumentOpenWorkflow() noexcept;
@@ -96,7 +99,7 @@ private:
     std::unique_ptr<player::tracks::application::AudioDelayController> audioDelayController_;
     std::unique_ptr<player::tracks::presentation::TrackListModel> audioTrackListModel_;
     std::unique_ptr<player::tracks::presentation::TrackListModel> subtitleTrackListModel_;
-    std::unique_ptr<player::tracks::presentation::ChapterModel> chapterModel_;
+    std::unique_ptr<player::chapters::presentation::ChapterModel> chapterModel_;
     std::unique_ptr<player::media::application::MediaOpenCoordinator> mediaOpenCoordinator_;
     std::unique_ptr<player::media::application::UrlOpenWorkflow> urlOpenWorkflow_;
     std::unique_ptr<player::media::application::MediaArgumentOpenWorkflow> mediaArgumentOpenWorkflow_;
